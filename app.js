9bbcf78025bad7f5f629dc0e48410ed74395b2e7
daniyalirfan1122 <mdaniyal.226688@gmail.com>
2024-07-31 17:48:44 -0700

    light

---
+++ app.js
@@ -0,0 +1,11 @@
+function end(){
+    var off = document.getElementById('off')
+    off.src ="img/bulbpic off.jpeg"
+    
+}
+
+function start(){
+    var on = document.getElementById('off')
+    on.src ="img/bulbpic on.jpeg"
+    
+}
